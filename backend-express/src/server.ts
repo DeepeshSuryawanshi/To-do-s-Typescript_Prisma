ea2cd9a49c79f647801ba6e385968ce6cee32f02
Deepesh suryawanshi <deepeshsuryawanshi04@gmail.com>
2025-01-25 14:24:31 +0530

    Delete backend-express directory
    not require old code

--- backend-express/src/server.ts
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-import app from './app.ts'
-import http from 'http';
-const server = http.createServer(app);
-server.listen(process.env.PORT,()=>{
-    console.log("server is running on Port 5000");
-})
